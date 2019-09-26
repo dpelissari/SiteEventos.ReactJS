792fc5fc3e13847884167295537baa8020bc27be
Daniel Pelissari <dpelissari1@gmail.com>
2019-09-26 14:49:38 -0300

    Add componente Menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import Logo from '../../../src/assets/img/logo.png';
+
+const Menu = () => (
+  <Navbar id="menu" expand="lg" sticky="top">
+  <Navbar.Brand href="#home"><img src={Logo} alt="logo" /></Navbar.Brand>
+  <Navbar.Toggle />
+  <Navbar.Collapse>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Programação</Nav.Link>
+      <Nav.Link href="#link">Palestrantes</Nav.Link>
+      <Nav.Link href="#link">Patrocinadores</Nav.Link>
+      <Nav.Link href="#link">Contato</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+)
+export default Menu;
